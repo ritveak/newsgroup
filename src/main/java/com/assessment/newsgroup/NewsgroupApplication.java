268c92e9a0413cb2059419514d0e274b997764a5
ritveak <26491325+ritveak@users.noreply.github.com>
2024-06-13 21:04:45 +0530

    initcode

---
+++ src/main/java/com/assessment/newsgroup/NewsgroupApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.newsgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsgroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/newsgroup/NewsgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.newsgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
