685561a89ecfd51bb48557d0ac8bea38cfb2a963
ritveak <26491325+ritveak@users.noreply.github.com>
2024-06-29 17:17:49 +0530

    Updated swagger configuration and added more validations

--- backend/src/main/java/com/assessment/newsgroup/controller/NewsController.java
+++ backend/src/main/java/com/assessment/newsgroup/controller/NewsController.java
@@ -6,11 +6,13 @@ import com.assessment.newsgroup.service.NewsApiService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,16 +28,16 @@ public class NewsController {
     @Operation(summary = "Search for news articles",
             description = "Search for news articles based on a keyword and group the results by date intervals")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
-            @ApiResponse(responseCode = "400", description = "Invalid input parameters"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list",    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponsePayload.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input parameters",    content = @Content(mediaType = "application/json", schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",    content = @Content(mediaType = "application/json", schema = @Schema(type = "string")))
     })
     @GetMapping("/search")
     public ResponsePayload searchNews(
             @Parameter(description = "Keyword to search for", required = true)
-            @RequestParam @NotBlank(message = "Keyword is mandatory") String keyword,
+            @RequestParam String keyword,
             @Parameter(description = "Interval for grouping results", required = false)
-            @RequestParam(required = false, defaultValue = "12") @Min(1) long interval,
+            @RequestParam(required = false, defaultValue = "12") long interval,
             @Parameter(description = "Unit for the interval", required = false, schema = @Schema(implementation = CustomChronoUnit.class))
             @RequestParam(required = false, defaultValue = "HOURS") String unit,
             @Parameter(description = "Is Offline mode or not", required = false)
@@ -43,6 +45,12 @@ public class NewsController {
     ) {
         if (!CustomChronoUnit.isValid(unit.toUpperCase())) {
             throw new IllegalArgumentException("Invalid unit: " + unit);
+        }
+        if(StringUtils.isEmpty(keyword)){
+            throw new IllegalArgumentException("Keyword cannot be empty, please retry with some value.");
+        }
+        if(interval<1){
+            throw new IllegalArgumentException("Interval cannot be less than 1, please retry with a valid number.");
         }
         CustomChronoUnit customChronoUnit = CustomChronoUnit.valueOf(unit.toUpperCase());
         return newsService.searchNews(keyword, interval, customChronoUnit, isOfflineMode);
